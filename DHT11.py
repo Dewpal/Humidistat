07a0fe5325335cbb50a00c11e24282b2618dea6d
Bram <bram.de.jaegher@gmail.com>
2016-09-18 23:24:02 +0200

    and DHT plot is LIVE

--- DHT11.py
+++ DHT11.py
@@ -21,6 +21,7 @@
 # SOFTWARE.
 import sys
 import time
+import datetime
 import Adafruit_DHT
 import plotly
 import pandas as pd
@@ -45,12 +46,34 @@ data= pd.DataFrame({'Time': 0,'Temperature': 0,'Humidity': 0}, columns=['Time','
 for i in range(0,nStore-1):
     humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
     if humidity is not None and temperature is not None:
-        data.loc[i]=pd.Series({'Time': 0,'Temperature': temperature,'Humidity': humidity}) 
+        data.loc[i]=pd.Series({'Time': datetime.datetime.now(),'Temperature': temperature,'Humidity': humidity}) 
     else:
         print('missed reading')
     print(data)
     time.sleep(Ts)
     i=i+1
 
-py.plot([trace],filename = 'basic_TH',)
+trace=go.Scatter(
+  x= data.Time,
+  y= data.Humidity,
+  stream=dict( 
+      token= "0f1psssxtu",
+      maxpoints= 200
+      )
+)
+
+layout = go.Layout(
+    title='RPi, DHT-sensor Data'
+)
+fig=go.Figure(data=[trace], layout=layout)
+
+print py.plot(fig,filename = 'basic_TH',)
       
+stream=py.Stream('0f1psssxtu')
+stream.open()
+
+while True:
+    humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+    i=i+1
+    stream.write({'x': datetime.datetime.now(), 'y': humidity})
+    time.sleep(Ts)
