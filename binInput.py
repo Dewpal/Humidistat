adc6f95dd9e66765906ec53eacacf6548559a824
Beramos <bram.de.jaegher@gmail.com>
2016-10-09 15:44:54 +0200

    solved issues controller and sensor are now on different threads

--- binInput.py
+++ binInput.py
@@ -5,114 +5,44 @@ import Adafruit_DHT
 import pandas as pd
 import datetime
 import time
-import Queue
-
-
-class virtualSystem:
-    def __init__(self,humidityFrame,dt):
-        self.humidityFrame=humidityFrame
-        self.dt=dt
-    return (100-humidity+humidity_prev)/dt
-
-def virtualSystem(humidity, humidity_prev,dt):
-
-
-class Controller:
-    def __init__(self,relayPin):
-        self.Ts=1   # (s) sampling time
-        self.LL=90  # lower limit humidity
-        self.relayPin=relayPin
-        def run (self):
-            global stopTime
-            global q
-            while time.clock() < stopTime:
-                if time.clock()-Time >= self.Ts:
-                    humidity=q.get()
-                    q.put(humidity)
-                    if #humidity on last step is < then:
-                        GPIO.output(relayPin,GPIO.LOW)
-                        print('Humidifier: on')
-                    else:
-                        GPIO.output(relayPin,GPIO.HIGH)
-                        print('Humidifier: off')
-                    Time=time.clock()
-                    print(Controller time:)
-                    print(Time) # debugging
-
-class Sensor:
-    def __init__(self,sensorPin,sensor):
-        global stopTime
-        self.Ts=10   # (s) sampling time
-        self.sensorPin=sensorPin
-        self.sensor=sensor
-        self.data=pd.DataFrame({'Time': 0,'Temperature': 0,'Humidity': 0, 'rTime':0}, columns=['Time','Temperature','Humidity','rTime'],index=range(0,round(stopTime/self.Ts)))
-        self.i=0
-        def run (self):
-            global q
-            global stopTime
-            while time.clock() < stopTime:
-                if time.clock()-Time >= self.Ts:
-                    humidity, temperature = Adafruit_DHT.read_retry(sensor, sensorPin)
-                    self.data.loc[i]=pd.Series({'Time': datetime.datetime.now(),'Temperature': temperature,'Humidity': humidity,'rTime':time.clock()})
-                    self.i+=1
-                    void=q.get()
-                    q.put(humidity)
-                    Time=time.clock()
-                print(Sensor time:)
-                print(Time) # debugging
-        return(self.data)
-# Global variables
-stopTime=60                 # (s)
-
-# Initialise virtualSystem
-humidityFrame=[40, 40] # current humidity and previous humidity
-
-if __name__ == "__main__":
-    q=Queue.Queue(1)
-
-    c=Controller(relayPin=26)
-    s=Sensor(sensorPin=21,sensor=Adafruit_DHT.DHT22)        # normally Adafruit_DHT.DHT22
-    v=virtualSystem(humidityFrame=humidityFrame,dt=s.Ts)    # must be the same as the sensor
-
 
 # Relay setup
-relayPin=26 # need to be set at the end
+relayPin=26
+
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(relayPin,GPIO.OUT)
 GPIO.output(relayPin,GPIO.HIGH) # K1 is off
 
 # DHT sensor setup
-sensorPin = 21 # needs to be set at the end
 sensor = Adafruit_DHT.DHT22
+sensorPin = 21
 
+# Step response parameters
+Ts=8 # sampling time
+steplength= 40 # length op step input
 
-
-# Initialisation
+# initialising arrays
+data= pd.DataFrame({'Time': 0,'Temperature': 0,'Humidity': 0, 'rTime':0}, columns=['Time','Temperature','Humidity','rTime'],index=range(0,steplength/Ts))
 i=1
 Time=0
-controlAction= list()
+In=pd.read_csv('bininp.csv',squeeze=True, header=0)
 
-
-# Controller
-for i in range(round(stopTime/Ts)):
+for i in range(steplength/Ts):
     n1=datetime.datetime.now()  # Start timing
-    humidity, temperature = Adafruit_DHT.read_retry(sensor, sensorPin)
 
-    if humidity < LL & len(controlAction) == 0
-
-
-
-            # Set output
+    if In[i] == 1:   # Set output
         GPIO.output(relayPin,GPIO.LOW)
         print('On')
+    else:
         GPIO.output(relayPin,GPIO.HIGH)
         print('Off')
+    humidity, temperature = Adafruit_DHT.read_retry(sensor, sensorPin)
     data.loc[i]=pd.Series({'Time': datetime.datetime.now(),'Temperature': temperature,'Humidity': humidity,'rTime':Time})
     i=i+1
     Time=Time+Ts
     n2=datetime.datetime.now()
     elapsedTime=(n2.microsecond-n1.microsecond)/1e6
-
+    
     if Ts-elapsedTime>0:
         time.sleep(Ts-elapsedTime)
 
